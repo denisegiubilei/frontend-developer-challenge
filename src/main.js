a2b5fff3605f9a04697f0a6d3869686ae525268c
denisegiubilei <giubileidenise@gmail.com>
2020-01-16 21:04:42 -0300

    constant with fallback img src

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ const API_ENDPOINT = 'https://frontend-intern-challenge-api.iurykrieger.now.sh'
 const LOCALE = 'pt-BR'
 const CURRENCY = 'BRL'
 const ADDTOCART_TEXT = 'Comprar'
+const IMG_FALLBACK_SRC = 'https://via.placeholder.com/200x200.jpg'
 
 window.onload = () => {
   populateProductList(0)
@@ -35,13 +36,13 @@ const populateProductList = page => {
       productContainer.innerHTML = products && Object.values(products).map(p =>
         `
           <div id="pid-${p.id}" class="card">
-            <img src="${p.image}" alt="${p.name}" class="product-img" onerror="this.src='https://via.placeholder.com/200x200.jpg'">
+            <img src="${p.image}" alt="${p.name}" class="product-img" onerror="this.src='${IMG_FALLBACK_SRC}'">
             <p>${p.name}</p>
             <p>${p.description}</p>
-            <p>${hasDiscount(p.oldPrice, p.price) ? 'De: ' + priceFmt(p.oldPrice) : ''}</p>
+            <p>${hasDiscount(p.oldPrice, p.price) && 'De: ' + priceFmt(p.oldPrice)}</p>
             <p class="price">${hasDiscount(p.oldPrice, p.price) ? 'Por: ' : ''}${priceFmt(p.price)}</p>
             <p>
-              ${hasInstallments(p.installments) ? 'ou ' + p.installments.count + 'x de ' + priceFmt(p.installments.value) : ''}
+              ${hasInstallments(p.installments) && 'ou ' + p.installments.count + 'x de ' + priceFmt(p.installments.value)}
             </p>
             <button class="addtocart button">${ADDTOCART_TEXT}</button>
           </div>
